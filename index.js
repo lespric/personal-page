5c6b3fa8462a9ec24abcda59582157f6b51173f7
lespric <704640923@qq.com>
2019-09-12 17:55:50 +0800

    commit message

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function openwindow(){
-    var written = document.getElementById('written');
+    var written = document.getElementById("written");
     //获取弹窗的板块
     var finish=document.getElementsByClassName("finish")[0];
     //获取弹窗中的完成按钮
@@ -7,7 +7,7 @@ function openwindow(){
     //获取弹窗中的关闭按钮
     written.style.display = "block";
     //窗体弹出
-    
+
     finish.onclick=function(){
         written.style.display = "none";
     } //点击完成后，窗体消失
